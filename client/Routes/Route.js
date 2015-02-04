8cdb09e9f25254370b1006fbd27c4886ded9d023
Darren Impey <darrenimpey@btinternet.com>
2015-02-04 21:58:15 +0000

    Deploy to heroku

--- client/Routes/Route.js
+++
@@ -1,7 +0,0 @@
-Router.map(function() {
-    this.route('Home', {path: '/'});
-    this.route('Banter', {path: '/Banter'});    
-    this.route('Scores', {path: '/Scores'});        
-    this.route('Settings', {path: '/Settings'});    
-    this.route('standingsList', {path: '/Standings'}); 
-});
