055074eab774ae3263d0df49bf8b46f8c7fe14e4
Darren Impey <darrenimpey@btinternet.com>
2015-02-04 23:18:02 +0000

    demeteored

---
+++ .demeteorized/programs/server/app/lib/router.js
@@ -0,0 +1,9 @@
+(function(){Router.map(function() {
+    this.route('Home', {path: '/'});
+    this.route('Banter', {path: '/Banter'});    
+    this.route('Scores', {path: '/Scores'});        
+    this.route('Settings', {path: '/Settings'});    
+    this.route('standingsList', {path: '/Standings'}); 
+});
+
+})();
